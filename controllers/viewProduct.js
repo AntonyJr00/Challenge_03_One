973ef8f9aa6f117133a27891ce75ec63c8f475fc
AntonyJr00 <antonyrodl20@gmail.com>
2023-07-18 11:32:26 -0500

    modificado en details de los productos

--- controllers/viewProduct.js
+++ controllers/viewProduct.js
@@ -1,37 +1,44 @@
-import {productosServicios} from "../servicios/productos-servicios.js";
-import {nuevoProducto} from "./productsController.js";
+import { productosServicios } from "../servicios/productos-servicios.js";
+import { nuevoProducto } from "./productsController.js";
 
-const imgProduct = document.querySelector('[data-img]');
-const nameProduct = document.querySelector('[data-name]');
-const priceProduct = document.querySelector('[data-price]');
-const descProduct = document.querySelector('[data-descripcion]');
+const imgProduct = document.querySelector("[data-img]");
+const nameProduct = document.querySelector("[data-name]");
+const priceProduct = document.querySelector("[data-price]");
+const descProduct = document.querySelector("[data-descripcion]");
 
 const url = new URL(window.location);
-const id = url.searchParams.get('id');
+const id = url.searchParams.get("id");
 
 const getInfo = async (id) => {
-    try {
-        const product = await productosServicios.detalleProducto(id);
+  try {
+    const product = await productosServicios.detalleProducto(id);
 
-        imgProduct.src = product.imageUrl;
-        nameProduct.textContent = product.name;
-        priceProduct.textContent = `precio: ${product.price}`;
-        descProduct.textContent = `Descripción del producto: ${product.description}`;
+    imgProduct.src = product.imageUrl;
+    nameProduct.textContent = product.name;
+    priceProduct.textContent = `precio: ${product.price}`;
+    descProduct.textContent = `Descripción del producto: ${product.description}`;
 
-        console.log({product, id});
+    const products = await productosServicios.listaProductos();
+    const allProducts = document.querySelector("[data-products]");
 
-        const products = await productosServicios.listaProductos();
-        const allProducts = document.querySelector('[data-products]');
-
-        allProducts.innerHTML = '';
-        products.filter(p => p.categoria === product.categoria).forEach(elemento => {
-            allProducts.appendChild((nuevoProducto(elemento.name, elemento.price, elemento.imageUrl, elemento.id)));
-        })
-    } catch (error) {
-        console.error("Hubo un Error", error);
-    }
+    allProducts.innerHTML = "";
+    products
+      .filter((p) => p.categoria === product.categoria)
+      .forEach((elemento) => {
+        allProducts.appendChild(
+          nuevoProducto(
+            elemento.name,
+            elemento.price,
+            elemento.imageUrl,
+            elemento.id
+          )
+        );
+      });
+  } catch (error) {
+    console.error("Hubo un Error", error);
+  }
 };
 
 if (id) {
-    getInfo(id);
+  getInfo(id);
 }
